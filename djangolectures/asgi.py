457646fe6dbd68f28d50205505a49ea347e156cb
Niels Hoppe <mail@nielshoppe.de>
2024-06-05 14:29:32 +0200

    Rename project

--- djangolectures/asgi.py
+++ djangolectures/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoexercises project.
+ASGI config for djangolectures project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoexercises.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangolectures.settings')
 
 application = get_asgi_application()
